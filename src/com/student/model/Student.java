b6792d6d01fd52f2b5c453302f29bd49bc46332c
mzm <mzm-0571@mail.ustc.edu.cn>
2024-05-09 18:11:11 +0800

    add details basically

--- src/com/student/model/Student.java
+++ src/com/student/model/Student.java
@@ -6,4 +6,11 @@ package com.student.model;
  * 学生实体类
  */
 public class Student {
+    private String id;
+    private String name;
+    private String sex;
+    private int age;
+    private String dept;
+    private String username;
+    private String password;
 }
