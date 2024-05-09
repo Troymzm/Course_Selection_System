2e946a58094793923d9e39429f6ba74f9cd5235d
mzm <mzm-0571@mail.ustc.edu.cn>
2024-05-09 18:13:45 +0800

    add details basically

--- src/com/student/model/Student.java
+++ src/com/student/model/Student.java
@@ -9,8 +9,59 @@ public class Student {
     private String id;
     private String name;
     private String sex;
-    private int age;
     private String dept;
     private String username;
     private String password;
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
