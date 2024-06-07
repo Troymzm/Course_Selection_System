1ca9a6b6a726eb2f6bc6e6bb698e72ad16f5ace5
mzm <mzm-0571@mail.ustc.edu.cn>
2024-06-08 00:31:27 +0800

    添加了课程人数上限属性

--- src/com/student/AppConstants.java
+++ src/com/student/AppConstants.java
@@ -1,5 +1,9 @@
 package com.student;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * @author mzm
  * @version 1.0
@@ -7,15 +11,24 @@ package com.student;
  * @Description 所有用到的常量
  */
 public class AppConstants {
+    static Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(new FileReader("src\\mysql.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     // JDBC
-    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String JDBC_URL = "jdbc:mysql://192.168.139.134:3306/course_selection";
-    public static final String JDBC_USERNAME = "819";
-    public static final String JDBC_PASSWORD = "819";
+    public static final String JDBC_DRIVER = properties.getProperty("JDBC_DRIVER");
+    public static final String JDBC_URL = properties.getProperty("JDBC_URL");
+    public static final String JDBC_USERNAME = properties.getProperty("JDBC_USERNAME");
+    public static final String JDBC_PASSWORD = properties.getProperty("JDBC_PASSWORD");
 
     //
     public static final String STUDENT_INITIAL_PASSWORD = "000000";
     public static final String ADMINISTRATOR_PASSWORD = "123456";
     public static final String ADMINISTRATOR_USERNAME = "administrator";
-
 }
